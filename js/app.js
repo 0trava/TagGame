22f8a8c8da6ce9b8960de505a2005e0c3c2f0ccb
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-08 22:09:04 +0300

    add

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const Btn = document.querySelector('.btn');
 console.log(Box);
 console.log(Btn);
 
-const Number = ['1', '2', '3','4','5','6','7','8','empty']
+let Number = ['1','2','3','4','5','6','7','8','a']
 
 
 for ( var i of Number) {
@@ -15,7 +15,21 @@ for ( var i of Number) {
 }
 
 function chackClick(e) {
-    console.log(e.target);
+    Emam = ['1','2','empty','3','4','5','6','7','8'];
+
+    for ( var i of Number) {
+        let BoxItem = document.getElementsByClassName(i);
+        Box.removeChild(BoxItem[0]);
+        console.log(BoxItem[0]);
+      }
+    
+      for ( var i of Emam) {
+        let BoxItem = document.createElement('div');
+        Box.appendChild(BoxItem);
+        BoxItem.classList.add(i);
+        BoxItem.innerText = i
+      }
+      
 
 }
 
