c7f9f82043f74a340da704c3438cc7d912de8d55
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-08 22:19:00 +0300

    add

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const Btn = document.querySelector('.btn');
 console.log(Box);
 console.log(Btn);
 
-let Number = ['1','2','3','4','5','6','7','8','a']
+let Number = ['1','2','3','4','5','6','7','8','empty']
 
 
 for ( var i of Number) {
@@ -15,23 +15,34 @@ for ( var i of Number) {
 }
 
 function chackClick(e) {
-    Emam = ['1','2','empty','3','4','5','6','7','8'];
+    const clickItem = e.target.classList[0];
+    console.log(clickItem);
 
+    let Index = Number.indexOf(clickItem);
+    console.log(Index);
+
+    // Emam = ['1','2','empty','3','4','5','6','7','8'];
+
+
+    // moveItem(Emam);
+
+}
+
+
+function moveItem(newArrey) {
     for ( var i of Number) {
         let BoxItem = document.getElementsByClassName(i);
         Box.removeChild(BoxItem[0]);
         console.log(BoxItem[0]);
       }
     
-      for ( var i of Emam) {
+      for ( var i of newArrey) {
         let BoxItem = document.createElement('div');
         Box.appendChild(BoxItem);
         BoxItem.classList.add(i);
         BoxItem.innerText = i
       }
-      
-
-}
+};
 
 Box.addEventListener('click', chackClick);
 
