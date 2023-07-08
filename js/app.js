4e42d145b36305e7119f3e5afe9e43c8c157040f
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-08 23:25:38 +0300

    add

--- js/app.js
+++ js/app.js
@@ -16,10 +16,8 @@ for ( var i of Number) {
 
 function chackClick(e) {
     const clickItem = e.target.classList[0];
-    console.log(clickItem);
 
     let Index = Number.indexOf(clickItem);
-    console.log(Index);
 
  switch (Index) {
     case 0:
@@ -137,40 +135,23 @@ function chackClick(e) {
        }
        break;
  }
-
-    // Emam = ['1','2','empty','3','4','5','6','7','8'];
-
-
     moveItem(Number);
     checkCollection(Number);
 
 }
 
 function checkCollection(checkArrey) {
-    if (checkArrey[0] === "1") {
-        if (checkArrey[1] === "2"){
-            if (checkArrey[2] === "3"){
-                if (checkArrey[3] === "4"){
-                    if (checkArrey[4] === "5"){
-                        if (checkArrey[5] === "6"){
-                            if (checkArrey[6] === "7"){
-                                if (checkArrey[7] === "8"){
-                                    console.log("win")
-                                }
-                                return;
-                            }
-                            return;
-                        }
-                        return;
-                    }
-                    return;
-                }
-                return;
-            }
-            return;
+
+for (let i = 0; i < 9; i++) {
+    let test = i + 1;
+   if (checkArrey[i] == test) {
+        if (test == 8) {
+            console.log("win")
         }
-        return;
-    }
+   } else {
+    i = 9;
+   }
+}
 }
 
 
@@ -178,7 +159,6 @@ function moveItem(newArrey) {
     for ( var i of Number) {
         let BoxItem = document.getElementsByClassName(i);
         Box.removeChild(BoxItem[0]);
-        console.log(BoxItem[0]);
       }
     
       for ( var i of newArrey) {
