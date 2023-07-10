9888b153b3c8ea7fe238b4a3fca359a2edebe69e
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-07-10 17:30:07 +0300

    add

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const Btn = document.querySelector('.btn');
 const winBox = document.querySelector('.box_win');
 
 
-let Number = ['1','2','3','4','5','6','7','8','empty']
+let Number = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','empty']
 
 
 for ( var i of Number) {
@@ -15,101 +15,108 @@ for ( var i of Number) {
 
 function chackClick(e) {
     const clickItem = e.target.classList[0];
-
+    
     let Index = Number.indexOf(clickItem);
-
+    console.log(Index);
  switch (Index) {
     case 0:
        if (Number[Index + 1] === "empty") { 
            Number[Index + 1] = clickItem; 
            Number[Index] = "empty"
        }
-       if (Number[Index + 3] === "empty") { 
-           Number[Index + 3] = clickItem; 
+       if (Number[Index + 4] === "empty") { 
+           Number[Index + 4] = clickItem; 
            Number[Index] = "empty";
        }
        break;
     case 1:
-        if (Number[Index - 1] === "empty") {
-            Number[Index - 1] = clickItem; 
-            Number[Index] = "empty"
-       }
-       if (Number[Index + 1] === "empty") { 
-           Number[Index + 1] = clickItem; 
-           Number[Index] = "empty"
-       }
-       if (Number[Index + 3] === "empty") { 
-           Number[Index + 3] = clickItem; 
-           Number[Index] = "empty";
-       }
-       break;
     case 2:
         if (Number[Index - 1] === "empty") {
             Number[Index - 1] = clickItem; 
             Number[Index] = "empty"
        }
-       if (Number[Index + 3] === "empty") { 
-           Number[Index + 3] = clickItem; 
+       if (Number[Index + 1] === "empty") { 
+           Number[Index + 1] = clickItem; 
+           Number[Index] = "empty"
+       }
+       if (Number[Index + 4] === "empty") { 
+           Number[Index + 4] = clickItem; 
            Number[Index] = "empty";
        }
        break;
     case 3:
-       if (Number[Index + 1] === "empty") { 
-           Number[Index + 1] = clickItem; 
-           Number[Index] = "empty"
-       }
-       if (Number[Index + 3] === "empty") { 
-           Number[Index + 3] = clickItem; 
-           Number[Index] = "empty";
-       }
-       if (Number[Index - 3] === "empty") {
-           Number[Index - 3] = clickItem; 
-           Number[Index] = "empty"
-       }
-       break;
-    case 4:
         if (Number[Index - 1] === "empty") {
             Number[Index - 1] = clickItem; 
             Number[Index] = "empty"
        }
+       if (Number[Index + 4] === "empty") { 
+           Number[Index + 4] = clickItem; 
+           Number[Index] = "empty";
+       }
+       break;
+    case 4:
+    case 8:
        if (Number[Index + 1] === "empty") { 
            Number[Index + 1] = clickItem; 
            Number[Index] = "empty"
        }
-       if (Number[Index + 3] === "empty") { 
-           Number[Index + 3] = clickItem; 
+       if (Number[Index + 4] === "empty") { 
+           Number[Index + 4] = clickItem; 
            Number[Index] = "empty";
        }
-       if (Number[Index - 3] === "empty") {
-           Number[Index - 3] = clickItem; 
+       if (Number[Index - 4] === "empty") {
+           Number[Index - 4] = clickItem; 
            Number[Index] = "empty"
        }
        break;
     case 5:
+    case 6:
+    case 9:
+    case 10:
         if (Number[Index - 1] === "empty") {
             Number[Index - 1] = clickItem; 
             Number[Index] = "empty"
        }
-       if (Number[Index + 3] === "empty") { 
-           Number[Index + 3] = clickItem; 
-           Number[Index] = "empty";
-       }
-       if (Number[Index - 3] === "empty") {
-           Number[Index - 3] = clickItem; 
-           Number[Index] = "empty"
-       }
-       break;
-    case 6:
        if (Number[Index + 1] === "empty") { 
            Number[Index + 1] = clickItem; 
            Number[Index] = "empty"
        }
-       if (Number[Index - 3] === "empty") {
-           Number[Index - 3] = clickItem; 
+       if (Number[Index + 4] === "empty") { 
+           Number[Index + 4] = clickItem; 
+           Number[Index] = "empty";
+       }
+       if (Number[Index - 4] === "empty") {
+           Number[Index - 4] = clickItem; 
            Number[Index] = "empty"
        }
        break;
     case 7:
+    case 11:
+        if (Number[Index - 1] === "empty") {
+            Number[Index - 1] = clickItem; 
+            Number[Index] = "empty"
+       }
+       if (Number[Index + 4] === "empty") { 
+           Number[Index + 4] = clickItem; 
+           Number[Index] = "empty";
+       }
+       if (Number[Index - 4] === "empty") {
+           Number[Index - 4] = clickItem; 
+           Number[Index] = "empty"
+       }
+       break;
+    case 12:
+       if (Number[Index + 1] === "empty") { 
+           Number[Index + 1] = clickItem; 
+           Number[Index] = "empty"
+       }
+       if (Number[Index - 4] === "empty") {
+           Number[Index - 4] = clickItem; 
+           Number[Index] = "empty"
+       }
+       break;
+    case 13:
+    case 14:
         if (Number[Index - 1] === "empty") {
             Number[Index - 1] = clickItem; 
             Number[Index] = "empty"
@@ -118,18 +125,18 @@ function chackClick(e) {
            Number[Index + 1] = clickItem; 
            Number[Index] = "empty"
        }
-       if (Number[Index - 3] === "empty") {
-           Number[Index - 3] = clickItem; 
+       if (Number[Index - 4] === "empty") {
+           Number[Index - 4] = clickItem; 
            Number[Index] = "empty"
        }
        break;
-    case 8:
+    case 15:
         if (Number[Index - 1] === "empty") {
             Number[Index - 1] = clickItem; 
             Number[Index] = "empty"
        }
-       if (Number[Index - 3] === "empty") {
-           Number[Index - 3] = clickItem; 
+       if (Number[Index - 4] === "empty") {
+           Number[Index - 4] = clickItem; 
            Number[Index] = "empty"
        }
        break;
@@ -144,7 +151,7 @@ function checkCollection(checkArrey) {
 for (let i = 0; i < 9; i++) {
     let test = i + 1;
    if (checkArrey[i] == test) {
-        if (test == 8) {
+        if (test == 14) {
             console.log("win");
             winBox.classList.remove('hidden')
 
